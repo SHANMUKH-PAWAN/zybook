5e9421345447a65a3bacaaff302ba05e18119248
Shanmukh Pawan Moparthi <smopart2@ncsu.edu>
2024-10-24 20:39:54 -0400

    Modified code to fix bugs

--- src/main/java/edu/ncsu/zybook/domain/model/Answer.java
+++ src/main/java/edu/ncsu/zybook/domain/model/Answer.java
@@ -12,6 +12,7 @@ public class Answer {
     private int sectionId;
     private int chapId;
     private int tbookId;
+    private int questionId;
     private String answerText;
     private String justification;
 }
--- src/main/java/edu/ncsu/zybook/domain/model/Textbook.java
+++ src/main/java/edu/ncsu/zybook/domain/model/Textbook.java
@@ -3,12 +3,9 @@ package edu.ncsu.zybook.domain.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 public class Textbook {
   int uid;
   String title;
-  List<Chapter> chapters;
 }
