d020aae0a7b99173e3555f14fa722874aeb4c78a
Naren-S-R <nsivaku6@ncsu.edu>
2024-10-25 01:52:19 -0400

    Added Section service, edited few other

--- src/main/java/edu/ncsu/zybook/persistence/repository/IContentRepository.java
+++ src/main/java/edu/ncsu/zybook/persistence/repository/IContentRepository.java
@@ -10,5 +10,5 @@ public interface IContentRepository {
     Optional<Content> update(Content content);
     boolean delete(int tbook_id, int chap_id, int section_id, int content_id);
     Optional<Content> findById(int id);
-    List<Content> findAll(int offset, int limit, String sortBy, String sortDirection, int tbook_id, int sectionId, int chapId);
+    List<Content> findAll(int offset, int limit, String sortBy, String sortDirection, int sectionId, int chapId, int tbook_id);
 }
