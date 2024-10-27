b0883ea3cbedf5aac5f8d1fd03eeb447cc643a66
Shanmukh Pawan Moparthi <smopart2@ncsu.edu>
2024-10-26 23:54:30 -0400

    Answer updated

--- src/main/java/edu/ncsu/zybook/domain/model/Answer.java
+++ src/main/java/edu/ncsu/zybook/domain/model/Answer.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Answer {
-    private int questionId;
-    private int answerId;
-    private int activityId;
-    private int contentId;
-    private int sectionId;
-    private int chapId;
-    private int tbookId;
+    private Integer questionId;
+    private Integer answerId;
+    private Integer activityId;
+    private Integer contentId;
+    private Integer sectionId;
+    private Integer chapId;
+    private Integer tbookId;
     private String answerText;
     private String justification;
 }
