5ede9c5d6b5346b261069ef20a294ffcef1cba09
Shanmukh Pawan Moparthi <smopart2@ncsu.edu>
2024-10-26 23:59:10 -0400

    merge conflicts reslevd

---
+++ src/main/java/edu/ncsu/zybook/service/ICourseService.java
@@ -0,0 +1,15 @@
+package edu.ncsu.zybook.service;
+
+import edu.ncsu.zybook.domain.model.Course;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICourseService {
+    Course create(Course entity);
+    Optional<Course> update(Course entity);
+    boolean delete(String id);
+    Optional<Course> findById(String id);
+    List<Course> findAll(int offset, int limit, String sortBy, String sortDirection);
+    Optional<Course> findByTitle(String title);
+}
